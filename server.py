fee16d5efed5e11e9c14485391769a6fcd02205e
moshiurtonmoy <18201027@uap-bd.edu>
2022-12-15 15:25:48 +0600

    Functionalities to rcv msg from clients added to Server side as well as Threading

--- server.py
+++ server.py
@@ -1,11 +1,43 @@
 #Imports
 import socket
+import threading
 
 #Constants
 HOST = '127.0.0.1'
 PORT = 1234
-CLIENT_LIMIT = 2
+CLIENT_LIMIT = 2    # Maximum connections to the server
+activeClients = []
 
+def clientHandler(client):
+    # waiting for client to join
+    while 1:
+        username = client.recv(32).decode('utf-8') # username max len is 32
+        if username != '':
+            activeClients.append((username,client)) # appending current to the active list
+            break;
+        else:
+            continue
+
+    threading.Thread(target=listeningMessages, args=(client,username,)).start()
+
+def listeningMessages(client, username):
+    # waiting for client to send msgs
+    while 1:
+        msg = client.recv(1024).decode('utf-8') # msg len max 1024 character
+        if msg != '' :
+            modifiedMsg = '[' + username + ']' + ' ~ ' + msg
+            sendMsgToAll(modifiedMsg)
+        else:
+            continue
+
+def sendMsgToAll(msg):
+    # Broadcasting msg to all active clients
+    for user in activeClients:
+        sendMsgToIndividual(user[1], msg)
+
+def sendMsgToIndividual(client, msg):
+    # Sending msg to individual
+    client.sendAll(msg.endcode())
 
 def main():
     # AF_NET - IPv4
@@ -26,6 +58,8 @@ def main():
     while 1 :
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=clientHandler, args=(client,)).start()
 
 if __name__ == '__main__':
     main()
